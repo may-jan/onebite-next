52e2c3514de053ad28c6576a01def7f73ab01bd9
jaeeun <jaeeun9862@gmail.com>
2024-09-12 00:27:12 +0900

    [Style] section2 스타일링

---
+++ section2/src/pages/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+// CSS Module
+var index_module_css_1 = require("./index.module.css");
+function Home() {
+    return (React.createElement(React.Fragment, null,
+        React.createElement("h1", { className: index_module_css_1["default"].h1 }, "\uC778\uB371\uC2A4"),
+        React.createElement("h2", { className: index_module_css_1["default"].h2 }, "H2")));
+}
+exports["default"] = Home;
