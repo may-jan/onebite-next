f0bd98864246c2cfdd029d2b4cf61283905e82bf
jaeeun <jaeeun9862@gmail.com>
2024-09-20 10:49:46 +0900

    [Chore] section2 SSR 실습

--- section2/src/pages/index.tsx
+++ section2/src/pages/index.tsx
@@ -1,21 +1,41 @@
 import SearchalbeLayout from '@/components/searchable-layout';
 import style from './index.module.css'; // CSS Module
 import { ReactNode } from 'react';
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import fetchBooks from '@/lib/fetch-books';
+import { InferGetServerSidePropsType } from 'next';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  // 컴포넌트보다 먼저 실행되어, 컴포넌트에 필요한 데이터를 불러오는 함수
+  // 서버 환경에서만 실행되는 함수
+
+  const [allBooks, recoBooks] = await Promise.all([
+    // Promise.all()
+    // 인수로 전달한 배열안에 들어있는 모든 비동기함수들을 동시에 실행시켜주는 함수
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  // return 값은 반드시 props라는 객체여야 한다
+  return { props: { allBooks, recoBooks } };
+};
+
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
--- section2/src/pages/search/index.tsx
+++ section2/src/pages/search/index.tsx
@@ -1,9 +1,27 @@
 import SearchalbeLayout from '@/components/searchable-layout';
 import { ReactNode } from 'react';
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from 'next';
+import fetchBooks from '@/lib/fetch-books';
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  // context : 현재 브라우저로 부터 받은 모든 정보가 포함
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+
+  return {
+    props: { books },
+  };
+};
+
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       {books.map((book) => (
