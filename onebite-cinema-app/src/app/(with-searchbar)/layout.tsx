3ca7935a847ca32d7030edf4c3838a72a3314aaf
jaeeun <jaeeun9862@gmail.com>
2024-09-25 17:03:22 +0900

    Day10 미션

--- onebite-cinema-app/src/app/(with-searchbar)/layout.tsx
+++ onebite-cinema-app/src/app/(with-searchbar)/layout.tsx
@@ -1,9 +1,10 @@
+import Searchbar from '@/components/searchbar';
 import { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <div>Searchbar Layout</div>
+      <Searchbar />
       {children}
     </div>
   );
---
+++ onebite-cinema-app/src/components/searchbar.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+export default function Searchbar() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [search, setSearch] = useState('');
+
+  const q = searchParams.get('q');
+
+  useEffect(() => {
+    setSearch(q || '');
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmitSearch = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDownSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onSubmitSearch();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDownSearch}
+        type='text'
+        placeholder='검색어를 입력하세요'
+      />
+      <button onSubmit={onSubmitSearch}>검색</button>
+    </div>
+  );
+}
