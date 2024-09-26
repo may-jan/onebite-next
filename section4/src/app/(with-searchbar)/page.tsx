cfcf0c19de5812a984a1902840022ab30d3ed70d
jaeeun <jaeeun9862@gmail.com>
2024-09-26 22:28:41 +0900

    [Chore] section4 데이터 캐시

--- section4/src/app/(with-searchbar)/page.tsx
+++ section4/src/app/(with-searchbar)/page.tsx
@@ -5,7 +5,8 @@ import { BookData } from '@/types';
 // 모든도서 fetching
 async function Allbooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: 'no-store' }
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>;
@@ -24,7 +25,8 @@ async function Allbooks() {
 // 추천도서 fetching
 async function RecoBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>;
