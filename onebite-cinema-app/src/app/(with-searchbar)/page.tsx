c3972f01d442eaa908719030d5ce830c6f2c8810
jaeeun <jaeeun9862@gmail.com>
2024-09-27 00:15:42 +0900

    Day12 미션

--- onebite-cinema-app/src/app/(with-searchbar)/page.tsx
+++ onebite-cinema-app/src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,31 @@
 import style from './page.module.css';
 import MovieItem from '@/components/movie-item';
 import movies from '../../mock/movies.json';
+import { MovieData } from '@/types';
+
+async function AllMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
+    { cache: 'force-cache' } // 새로운 영화를 추가 or 삭제하지 않기 때문에
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>;
+  }
+  const movies: MovieData[] = await response.json();
+  return movies.map((movie) => <MovieItem key={movie.id} {...movie} />);
+}
+
+async function RecoMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
+    { next: { revalidate: 3 } } // 추천 영화를 3초마다 업데이트하기 위해
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>;
+  }
+  const movies: MovieData[] = await response.json();
+  return movies.map((movie) => <MovieItem key={movie.id} {...movie} />);
+}
 
 export default function Home() {
   return (
@@ -8,17 +33,13 @@ export default function Home() {
       <section>
         <h3>지금 가장 추천하는 영화</h3>
         <div className={style.recommend_movie}>
-          {movies.slice(0, 3).map((movie) => (
-            <MovieItem key={movie.id} {...movie} />
-          ))}
+          <RecoMovies />
         </div>
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
         <div className={style.all_movie}>
-          {movies.map((movie) => (
-            <MovieItem key={movie.id} {...movie} />
-          ))}
+          <AllMovies />
         </div>
       </section>
     </div>
--- onebite-cinema-app/src/app/(with-searchbar)/search/page.tsx
+++ onebite-cinema-app/src/app/(with-searchbar)/search/page.tsx
@@ -1,13 +1,25 @@
-import movies from '@/mock/movies.json';
+import { MovieData } from '@/types';
 import style from './page.module.css';
 import MovieItem from '@/components/movie-item';
 
-export default function Page({
+export default async function Page({
   searchParams,
 }: {
   searchParams: { q?: string };
 }) {
   const q = searchParams.q;
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${q}`,
+    { cache: 'force-cache' } // 동일한 검색어는 동일한 정보를 가져오기 때문에
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>;
+  }
+
+  const movies: MovieData[] = await response.json();
+
   return (
     <div className={style.container}>
       {movies.map((movie) => (
