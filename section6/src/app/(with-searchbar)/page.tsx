866b07ef203ab100869afd7a6d031e5bf9e10a4d
jaeeun <jaeeun9862@gmail.com>
2024-09-28 23:08:02 +0900

    [Chore] section6 페이지 스트리밍 적용하기

---
+++ section6/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,66 @@
+import BookItem from '@/components/book-item';
+import style from './page.module.css';
+import { BookData } from '@/types';
+
+// export const dynamic = 'force-dynamic';
+// 특정 페이지의 유형을 강제로 Static or Dynamic 페이지로 설정
+// 1. auto : 기본값, 아무것도 강제하지 않음
+// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
+// 3. force-static : 페이지를 강제로 Static 페이지로 설정
+//                   페이지 내부에서 사용한 동적함수는 자동으로 undefined 반환
+// 4. error : 페이지를 강제로 Static 페이지로 설정 (Static으로 설정하면 안되는 이유가 있다면 빌드 오류를 발생)
+
+// 모든도서 fetching
+async function Allbooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: 'force-cache' }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>;
+  }
+
+  const allBooks: BookData[] = await response.json();
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+// 추천도서 fetching
+async function RecoBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>;
+  }
+
+  const recoBooks: BookData[] = await response.json();
+  return (
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <RecoBooks />
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <Allbooks />
+      </section>
+    </div>
+  );
+}
