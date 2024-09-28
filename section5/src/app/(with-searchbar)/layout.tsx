a8f5c9b2ed22c00f4c0f3dd4b7cc7b147d7ed4f3
jaeeun <jaeeun9862@gmail.com>
2024-09-28 22:08:11 +0900

    [Chore] section5 클라이언트 라우터 캐시

--- section5/src/app/(with-searchbar)/layout.tsx
+++ section5/src/app/(with-searchbar)/layout.tsx
@@ -4,6 +4,8 @@ import Searchbar from '../../components/searchbar';
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
+      {/* 클라이언트 라우트 캐시 */}
+      {/* <div>{new Date().toLocaleString()}</div> */}
       <Suspense fallback={<div>Loading ...</div>}>
         <Searchbar />
       </Suspense>
