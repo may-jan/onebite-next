175665fa86df3159ca2de06791b353375320389b
jaeeun <jaeeun9862@gmail.com>
2024-10-02 22:37:18 +0900

    [Chore] section7 클라이언트 컴포넌트에서의 서버액션

--- section7/src/actions/create-review.action.ts
+++ section7/src/actions/create-review.action.ts
@@ -1,14 +1,19 @@
 'use server';
 
+import { delay } from '@/util/delay';
 import { revalidatePath, revalidateTag } from 'next/cache';
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
+  await delay(2000);
   const bookId = formData.get('bookId')?.toString();
   const content = formData.get('content')?.toString();
   const author = formData.get('author')?.toString();
 
-  if (!content || !author) {
-    return;
+  if (!bookId || !content || !author) {
+    return {
+      status: false,
+      error: '리뷰 내용과 작성자를 입력해주세요',
+    };
   }
 
   try {
@@ -19,7 +24,10 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       }
     );
-    console.log(response.status);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
 
     // 1. 특정 주소의 해당하는 페이지만 재검증
     // revalidatePath(`/book/${bookId}`);
@@ -35,8 +43,15 @@ export async function createReviewAction(formData: FormData) {
 
     // 5. 태그 기준, 데이터 캐시 재검증
     revalidateTag(`review-${bookId}`);
+
+    return {
+      status: true,
+      error: '',
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${err}`,
+    };
   }
 }
